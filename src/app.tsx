a7871a9c785dc6a4491074ebf2da90676ee48cff
Lisandro Olivares <116845904+mxlisandro@users.noreply.github.com>
2023-11-21 21:01:59 -0600

    Created new branch and made basic setup

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Dashboard from './pages/Dashboard'
+
+const App: React.FC = () => {
+  return (
+    <div>
+        <Dashboard />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard: React.FC = () => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
