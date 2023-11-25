caca5853f8e3250791b662c41e49f6f1cc9c496c
Lisandro Olivares <116845904+mxlisandro@users.noreply.github.com>
2023-11-25 13:31:33 -0600

    added tailwind, header, sidebar and addWallet component contents

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,38 @@
 import React from 'react'
 import pfp from '../img/pfp.png'
 
-type Props = {}
+type Props = {
+    username: string;
+}
 
 const Header = (props: Props) => {
   return (
-    <div>Header
+    <header>
+    <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+        <div className="text-center sm:text-left">
+            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+            EtherView
+            </h1>
+        </div>
+        <div className="text-center sm:text-left">
+            <h1 className="text-2xl text-gray-900 sm:text-s">
+            {props.username} Dasboard
+            </h1>
+        </div>
 
-    <img alt='profile' src={pfp} />
+
+        <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
+            <button
+            className="block rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring"
+            type="button"
+            >
+            Logout
+            </button>
+        </div>
+        </div>
     </div>
+    </header>
   )
 }
 
