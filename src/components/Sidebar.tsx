7dca8497d3d9f0d95fc75fd3bc1346a930c6907a
Lisandro Olivares <116845904+mxlisandro@users.noreply.github.com>
2023-11-27 19:16:13 -0600

    Corrected mistakes before pull request merge

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React, {Dispatch, SetStateAction} from 'react'
-import walletIco from '../img/wallet.png'
+import walletIcon from '../img/wallet.png'
 import trash from '../img/delete.png'
 import add from '../img/add.png'
 import addWallet from './addWallet'
@@ -46,7 +46,7 @@ const Sidebar = (props: Props) => {
                     <button
               className="group relative flex justify-center rounded px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700"
             >
-                <img src={walletIco} alt='wallet'/>
+                <img src={walletIcon} alt='wallet'/>
               <span
                 className="absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white opacity-0 group-hover:opacity-100"
               >
